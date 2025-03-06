f2d15a6a9a77719198b1dab95b4b474eb39932be
Samir <samir.mangroliya@zoodmall.com>
2025-03-06 16:53:26 +0530

    NavigationRail, SearchBar, SwipeToDismissExample, EditTextExample

--- app/src/main/java/com/samir/composeonly/composeExample/SwipeToDismissExample.kt
+++ app/src/main/java/com/samir/composeonly/composeExample/SwipeToDismissExample.kt
@@ -3,8 +3,6 @@ package com.samir.composeonly.composeExample
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
